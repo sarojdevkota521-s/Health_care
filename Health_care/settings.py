dd95dbdd20cb26c3cfbc388d9d8f733f0a9fb9ab
saroj <sarojdevkota521@gmail.com>
2026-02-22 15:39:56 +0545

    postgresql updated

--- Health_care/settings.py
+++ Health_care/settings.py
@@ -76,10 +76,21 @@ AUTH_USER_MODEL = 'account.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Health_care',
+        'USER': 'postgres',
+        'PASSWORD': 'saroj@12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
